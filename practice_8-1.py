853ecf6bbdb94948a69e4e1dd49d87749a75abce
Hongjeongin <64721629+Hongjeongin@users.noreply.github.com>
2022-10-20 19:38:27 +0900

    practice Loop

---
+++ practice_8-1.py
@@ -0,0 +1,50 @@
+#131 
+fruits = ["사과", "귤", "수박"]
+for val in fruits:
+    print(val)
+
+#132
+for val in fruits:
+    print("#####")
+
+#133 for문 풀어쓰기
+print("A")
+print("B")
+print("C")
+
+#134 for문 풀어쓰기
+print("출력: A")
+print("출력: B")
+print("출력: C")
+
+#135 for문 풀어쓰기
+alphabets = ["A", "B", "C"]
+low_al = alphabets[0].lower()
+print(low_al)
+low_al = alphabets[1].lower()
+print(low_al)
+low_al = alphabets[2].lower()
+print(low_al)
+
+#136 for문 작성
+variable = [10, 20, 30]
+for val in variable:
+    print(val)
+
+#137 for문 작성
+for val in range(10, 31, 10):
+    print(val)
+
+#138 for문 작성
+for val in range(10, 31, 10):
+    print(val)
+    print("-------")
+
+#139 for문 작성
+print("++++")
+for val in range(10, 31, 10):
+    print(val)
+
+#140 for문 작성
+for i in range(0, 4):
+    print("-------")
